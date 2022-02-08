eea8c31cd23261271baf06b9521bc41fafcc65e4
Mario <mariob360@gmail.com>
2022-02-08 13:37:24 -0800

    create logo with base styled-components

---
+++ src/components/logo/Logo.js
@@ -0,0 +1,7 @@
+import logo from "../../assets/desktop/logo.svg";
+
+const Logo = () => {
+  return <img src={logo} alt="Pod Home" />;
+};
+
+export default Logo;
---
+++ src/components/logo/LogoStyles.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+const LogoStyles = styled.img``;
+
+export default LogoStyles;
