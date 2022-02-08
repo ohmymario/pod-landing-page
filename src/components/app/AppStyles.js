ee057a74f827f4ad725f36019a496e4e6fa27972
Mario <mariob360@gmail.com>
2022-02-07 18:39:34 -0800

    add styles to app component

---
+++ src/components/app/AppStyles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+
+const AppStyles = styled.main`
+  max-width: 1440px;
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+  padding-bottom: 12rem;
+  padding-top: 7.75rem;
+`;
+
+export default AppStyles;
