2b8d34d739d2a16fb2cf86d10bdba816573ac80e
Mario <mariob360@gmail.com>
2022-02-09 15:44:20 -0800

    matched companies colors

--- src/components/companies/CompaniesStyles.js
+++ src/components/companies/CompaniesStyles.js
@@ -10,6 +10,10 @@ const CompaniesStyles = styled.div`
   img {
     display: block;
 
+    /* used https://codepen.io/sosuke/pen/Pjoqqp?editors=0110 */
+    filter: brightness(0) saturate(100%) invert(41%) sepia(15%) saturate(957%)
+      hue-rotate(187deg) brightness(91%) contrast(90%);
+
     &:not(:last-child) {
       margin-right: 40px;
     }
