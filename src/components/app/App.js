73f719f7e4deceae258e0a084a3b968631476532
Mario <mariob360@gmail.com>
2022-02-08 16:17:42 -0800

    add companies into app component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,12 +2,14 @@ import AppStyles from "./AppStyles";
 import Logo from "../logo/Logo";
 import PodInfo from "../podInfo/PodInfo";
 import Attribution from "../attribution/Attribution";
+import Companies from "../companies/Companies";
 
 function App() {
   return (
     <AppStyles>
       <Logo />
       <PodInfo />
+      <Companies />
       <Attribution />
     </AppStyles>
   );
