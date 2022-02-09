509bc646535f8006c8fbb0e0d369103208ffdf74
Mario <mariob360@gmail.com>
2022-02-09 15:20:08 -0800

    add styles to companies component

--- src/components/companies/CompaniesStyles.js
+++ src/components/companies/CompaniesStyles.js
@@ -1,5 +1,23 @@
 import styled from "styled-components";
 
-const CompaniesStyles = styled.div``;
+// 29px height
+// ~ 40 px in between each
+
+const CompaniesStyles = styled.div`
+  display: flex;
+  align-items: center;
+
+  img {
+    display: block;
+
+    &:not(:last-child) {
+      margin-right: 40px;
+    }
+
+    &:nth-child(3) {
+      margin-right: 27px;
+    }
+  }
+`;
 
 export default CompaniesStyles;
