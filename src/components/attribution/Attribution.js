0e11270d736a19712ea54c5943fd4a6d05fdaaee
Mario <mariob360@gmail.com>
2022-02-08 16:23:17 -0800

    add attribution base component

---
+++ src/components/attribution/Attribution.js
@@ -0,0 +1,19 @@
+import AttrStyles from "./AttributionStyles";
+
+const Attribution = () => {
+  return (
+    <AttrStyles>
+      Challenge by
+      <a
+        href="https://www.frontendmentor.io?ref=challenge"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by <a href="#">Mario Ballesteros</a>.
+    </AttrStyles>
+  );
+};
+
+export default Attribution;
---
+++ src/components/attribution/AttributionStyles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const AttrStyles = styled.footer`
+   {
+    /* font-family: var(--body-font); */
+    font-size: 11px;
+    text-align: center;
+    letter-spacing: 0.5px;
+    /* color: var(--white); */
+    a {
+      /* color: var(--white); */
+      transition: all 0.35s;
+    }
+  }
+`;
+
+export default AttrStyles;
