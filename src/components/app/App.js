725e0f47cd161e066b019e5ce37ebdc0e45e1e11
Mario <mariob360@gmail.com>
2022-02-09 19:33:09 -0800

    add desktop image host into App

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,15 +2,14 @@ import AppStyles from "./AppStyles";
 import Logo from "../logo/Logo";
 import PodInfo from "../podInfo/PodInfo";
 import Attribution from "../attribution/Attribution";
-import Companies from "../companies/Companies";
 
 function App() {
   return (
     <AppStyles>
       <Logo />
       <PodInfo />
-      <Companies />
-      <Attribution />
+      {/* TEMP REMOVAL TO GET BASE STYLES DONE */}
+      {/* <Attribution /> */}
     </AppStyles>
   );
 }
--- src/components/app/AppStyles.js
+++ src/components/app/AppStyles.js
@@ -1,14 +1,13 @@
 import styled from "styled-components";
+import desktop from "../../assets/desktop/image-host.jpg";
 
 const AppStyles = styled.div`
   max-width: 1440px;
   margin: 0 auto;
-
   width: 100%;
   height: 100%;
-  padding-top: 100px;
-
-  border: 1px solid red;
+  padding-top: 102px;
+  position: relative;
 
   /* img from top 130px */
   /* 888x640 img full size on desktop */
@@ -16,6 +15,20 @@ const AppStyles = styled.div`
   /* dots 718 from top */
   /* dots touching right side */
   /* dots 78 from bottom */
+
+  &:before {
+    content: "";
+    position: absolute;
+    z-index: -1;
+    top: 130px;
+    width: 100%;
+    height: 640px;
+    background-image: url(${desktop});
+    background-position: top right;
+    background-repeat: no-repeat;
+  }
+  &:after {
+  }
 `;
 
 export default AppStyles;
