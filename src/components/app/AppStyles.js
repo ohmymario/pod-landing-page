3a1b759f94cca7280028199fa9c0d2a09a2db692
Mario <mariob360@gmail.com>
2022-02-10 18:36:03 -0800

    Add tablet view to app component

--- src/components/app/AppStyles.js
+++ src/components/app/AppStyles.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import desktop from "../../assets/desktop/image-host.jpg";
+import tablet from "../../assets/tablet/image-host.jpg";
 import dots from "../../assets/desktop/bg-pattern-dots.svg";
 
 const AppStyles = styled.div`
@@ -27,15 +28,36 @@ const AppStyles = styled.div`
     background-image: url(${desktop});
     background-position: top right;
     background-repeat: no-repeat;
+
+    @media (max-width: 1000px) {
+      background-image: url(${tablet});
+      height: 100%;
+      top: 0;
+    }
   }
   &:after {
     content: url(${dots});
     position: absolute;
     z-index: -1;
     right: 0;
-
     /* half the size of the dots */
     bottom: -52px;
+
+    @media (max-width: 1000px) {
+      position: fixed;
+      left: 30px;
+      bottom: 0;
+    }
+  }
+
+  @media (max-width: 1440px) {
+    width: initial;
+    margin: 0 20px;
+  }
+
+  @media (max-width: 1000px) {
+    margin: 0 0 0 40px;
+    padding-top: 50px;
   }
 `;
 
